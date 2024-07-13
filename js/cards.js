a8a1c63bffb9848cfda5e8d13436449048213a1e
PGAT begginer <137990341+hachchch@users.noreply.github.com>
2024-07-13 11:17:01 +0900

    Create cards.js

---
+++ js/cards.js
@@ -0,0 +1,26 @@
+class cards{
+  name;
+  power;
+  cost;
+  field;
+  uniqueEffect;
+  message;
+  setName(name) {
+    this.name = name;
+  }
+  setPower(power) {
+    this.power = power;
+  }
+  setCost(cost) {
+    this.cost = cost;
+  }
+  setField(field) {
+    this.field = field;
+  }
+  setUniqueEffect(uniqueEffect) {
+    this.uniqueEffect = uniqueEffect;
+  }
+  setMessage(message) {
+    this.message = message;
+  }
+}
